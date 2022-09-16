4689151f59bc416d79ee1a00721b470755d52873
Low Kok Wei <98858885+lowkw@users.noreply.github.com>
2022-09-16 10:13:19 +0800

    Add files via upload

---
+++ AstronomicalProcessing/AstronomicalProcessing/AstronomicalProcessing.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace AstronomicalProcessing
+{
+    partial class AstronomicalProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbStarVelocityObservedWaveIn = new System.Windows.Forms.TextBox();
+            this.tbStarVelocityOut = new System.Windows.Forms.TextBox();
+            this.tbStarDistanceIn = new System.Windows.Forms.TextBox();
+            this.tbStarDistanceOut = new System.Windows.Forms.TextBox();
+            this.tbStarVelocityRestWaveIn = new System.Windows.Forms.TextBox();
+            this.tbTemperatureIn = new System.Windows.Forms.TextBox();
+            this.tbTemperatureOut = new System.Windows.Forms.TextBox();
+            this.tbEventHorizonBlackholeIn = new System.Windows.Forms.TextBox();
+            this.tbEventHorizonPowerIn = new System.Windows.Forms.TextBox();
+            this.tbEventHorizonOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbStarVelocityObservedWaveIn
+            // 
+            this.tbStarVelocityObservedWaveIn.Location = new System.Drawing.Point(102, 44);
+            this.tbStarVelocityObservedWaveIn.Name = "tbStarVelocityObservedWaveIn";
+            this.tbStarVelocityObservedWaveIn.Size = new System.Drawing.Size(100, 20);
+            this.tbStarVelocityObservedWaveIn.TabIndex = 0;
+            // 
+            // tbStarVelocityOut
+            // 
+            this.tbStarVelocityOut.Location = new System.Drawing.Point(332, 43);
+            this.tbStarVelocityOut.Name = "tbStarVelocityOut";
+            this.tbStarVelocityOut.Size = new System.Drawing.Size(100, 20);
+            this.tbStarVelocityOut.TabIndex = 1;
+            // 
+            // tbStarDistanceIn
+            // 
+            this.tbStarDistanceIn.Location = new System.Drawing.Point(102, 111);
+            this.tbStarDistanceIn.Name = "tbStarDistanceIn";
+            this.tbStarDistanceIn.Size = new System.Drawing.Size(100, 20);
+            this.tbStarDistanceIn.TabIndex = 2;
+            // 
+            // tbStarDistanceOut
+            // 
+            this.tbStarDistanceOut.Location = new System.Drawing.Point(332, 111);
+            this.tbStarDistanceOut.Name = "tbStarDistanceOut";
+            this.tbStarDistanceOut.Size = new System.Drawing.Size(100, 20);
+            this.tbStarDistanceOut.TabIndex = 3;
+            // 
+            // tbStarVelocityRestWaveIn
+            // 
+            this.tbStarVelocityRestWaveIn.Location = new System.Drawing.Point(102, 71);
+            this.tbStarVelocityRestWaveIn.Name = "tbStarVelocityRestWaveIn";
+            this.tbStarVelocityRestWaveIn.Size = new System.Drawing.Size(100, 20);
+            this.tbStarVelocityRestWaveIn.TabIndex = 4;
+            // 
+            // tbTemperatureIn
+            // 
+            this.tbTemperatureIn.Location = new System.Drawing.Point(102, 173);
+            this.tbTemperatureIn.Name = "tbTemperatureIn";
+            this.tbTemperatureIn.Size = new System.Drawing.Size(100, 20);
+            this.tbTemperatureIn.TabIndex = 5;
+            // 
+            // tbTemperatureOut
+            // 
+            this.tbTemperatureOut.Location = new System.Drawing.Point(332, 172);
+            this.tbTemperatureOut.Name = "tbTemperatureOut";
+            this.tbTemperatureOut.Size = new System.Drawing.Size(100, 20);
+            this.tbTemperatureOut.TabIndex = 6;
+            // 
+            // tbEventHorizonBlackholeIn
+            // 
+            this.tbEventHorizonBlackholeIn.Location = new System.Drawing.Point(102, 236);
+            this.tbEventHorizonBlackholeIn.Name = "tbEventHorizonBlackholeIn";
+            this.tbEventHorizonBlackholeIn.Size = new System.Drawing.Size(100, 20);
+            this.tbEventHorizonBlackholeIn.TabIndex = 7;
+            // 
+            // tbEventHorizonPowerIn
+            // 
+            this.tbEventHorizonPowerIn.Location = new System.Drawing.Point(102, 281);
+            this.tbEventHorizonPowerIn.Name = "tbEventHorizonPowerIn";
+            this.tbEventHorizonPowerIn.Size = new System.Drawing.Size(100, 20);
+            this.tbEventHorizonPowerIn.TabIndex = 8;
+            // 
+            // tbEventHorizonOut
+            // 
+            this.tbEventHorizonOut.Location = new System.Drawing.Point(332, 235);
+            this.tbEventHorizonOut.Name = "tbEventHorizonOut";
+            this.tbEventHorizonOut.Size = new System.Drawing.Size(100, 20);
+            this.tbEventHorizonOut.TabIndex = 9;
+            // 
+            // AstronomicalProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbEventHorizonOut);
+            this.Controls.Add(this.tbEventHorizonPowerIn);
+            this.Controls.Add(this.tbEventHorizonBlackholeIn);
+            this.Controls.Add(this.tbTemperatureOut);
+            this.Controls.Add(this.tbTemperatureIn);
+            this.Controls.Add(this.tbStarVelocityRestWaveIn);
+            this.Controls.Add(this.tbStarDistanceOut);
+            this.Controls.Add(this.tbStarDistanceIn);
+            this.Controls.Add(this.tbStarVelocityOut);
+            this.Controls.Add(this.tbStarVelocityObservedWaveIn);
+            this.Name = "AstronomicalProcessing";
+            this.Text = "AstronomicalProcessing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbStarVelocityObservedWaveIn;
+        private System.Windows.Forms.TextBox tbStarVelocityOut;
+        private System.Windows.Forms.TextBox tbStarDistanceIn;
+        private System.Windows.Forms.TextBox tbStarDistanceOut;
+        private System.Windows.Forms.TextBox tbStarVelocityRestWaveIn;
+        private System.Windows.Forms.TextBox tbTemperatureIn;
+        private System.Windows.Forms.TextBox tbTemperatureOut;
+        private System.Windows.Forms.TextBox tbEventHorizonBlackholeIn;
+        private System.Windows.Forms.TextBox tbEventHorizonPowerIn;
+        private System.Windows.Forms.TextBox tbEventHorizonOut;
+    }
+}
+
---
+++ AstronomicalProcessing/AstronomicalProcessing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+// Low, Kok Wei (M214391)
+
+namespace AstronomicalProcessing
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AstronomicalProcessing());
+
+            ChannelFactory<IAstroContract> pipeFactory =
+                new ChannelFactory<IAstroContract>(
+                    new NetNamedPipeBinding(),
+                    new EndpointAddress("net.pipe://localhost/PipeAstro"));
+
+            IAstroContract pipeProxy = pipeFactory.CreateChannel();
+        }
+    }
+}
